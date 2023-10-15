03d0bcdab17a8254a83d25e1bd893192e38535a5
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-10-15 18:52:21 +0100

    dynamic segments

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,4 +1,5 @@
 import axios from "axios"
+import Link from "next/link";
 async function getTickets(){
    const res= await axios.get('http://localhost:4000/tickets',{
     next:{
@@ -16,11 +17,13 @@ export default async function ticketList() {
     <>
       {tickets.map((ticket)=>(
         <div key={ticket.id} className="card my-5">
+           <Link href={`/tickets/${ticket.id}`}>
             <h3>{ticket.title}</h3>
             <p>{ticket.body.slice(0,200)}...</p>
             <div className={`pill ${ticket.priority}`}>
                 {ticket.priority} priority
             </div>
+            </Link>
         </div>
       ))
       }
@@ -30,3 +33,6 @@ export default async function ticketList() {
     </>
   )
 }
+
+
+///tickets/id
