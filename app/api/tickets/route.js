70eac03983ec1e0eb5ad3f941144fe32411fa7dd
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-10-16 23:33:06 +0100

    dynamic vs route hundlers

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -1,6 +1,9 @@
 // /api/tickets 
 import axios from "axios";
 import { NextResponse } from "next/server";
+
+export const dynamis='force-dynamic'
+
 export async function GET(){
   const res= await axios.get('http://localhost:4000/tickets')
   const tickets=await res.data
