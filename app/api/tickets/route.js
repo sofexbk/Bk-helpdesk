7aec40eab0895b4d160fa70fed1c999929ac0ea1
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-10-16 23:26:50 +0100

    get request handled => pstmn 3000

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -1,2 +1,11 @@
 // /api/tickets 
+import axios from "axios";
+import { NextResponse } from "next/server";
+export async function GET(){
+  const res= await axios.get('http://localhost:4000/tickets')
+  const tickets=await res.data
 
+  return NextResponse.json(tickets,{
+    status:200 
+  })
+}
