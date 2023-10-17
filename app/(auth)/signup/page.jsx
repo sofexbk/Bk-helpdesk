48a36264b88f714973a9239f432cbeeb808f512f
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-10-17 08:05:20 +0100

    auth form done

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,9 +1,17 @@
-
+"use client"
+import AuthForm from "../AuthForm"
 
 export default function Signup() {
+
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
+
   return (
     <main>
-       <h2 className="text-center">Sign up</h2>
+      <h2 className="text-center">Sign up</h2>
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   )
 }
