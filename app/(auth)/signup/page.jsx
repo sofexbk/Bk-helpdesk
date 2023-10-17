7279b3a2c8096289c50689c7af02882fbc4deb5d
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-10-17 20:45:48 +0100

    signing up a new user

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,17 +1,39 @@
 "use client"
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+
 import AuthForm from "../AuthForm"
+import { useState } from "react"
+import { useRouter } from 'next/navigation'
 
 export default function Signup() {
+  const [error, setError] = useState('')
+  const router =useRouter()
 
   const handleSubmit = async (e, email, password) => {
     e.preventDefault()
-    console.log(email, password)
+    const supbase=createClientComponentClient() 
+    const {error}=await supbase.auth.signUp({
+      email, 
+      password,
+      options:{
+        emailRedirectTo:`${location.origin}/api/auth/callback`
+      }
+    })
+    if(error){
+      setError(error.message)
+    }else{
+      router.push('/verify')
+    }
   }
 
   return (
     <main>
       <h2 className="text-center">Sign up</h2>
       <AuthForm handleSubmit={handleSubmit} />
+      {error && (
+        <div className="error">{error}</div>
+      ) }
     </main>
   )
 }
--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={rubik.className}>
-        {children}
+        {children} 
         </body>
     </html>
   )
