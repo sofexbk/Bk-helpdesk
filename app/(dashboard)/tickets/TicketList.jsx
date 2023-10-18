9c1ed06823cb2831a4c7cbca8e25eb4d0c165ccc
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-10-18 19:13:07 +0100

    issue

--- app/(dashboard)/tickets/TicketList.jsx
+++ app/(dashboard)/tickets/TicketList.jsx
@@ -1,45 +1,53 @@
-import Link from "next/link"
+import { notFound } from 'next/navigation'
 import { cookies } from 'next/headers'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 
+export const dynamicParams = true
 
-async function getTickets() {
+export async function generateMetadata({ params }) {
   const supabase = createServerComponentClient({ cookies })
 
-  const { data, error } = await supabase.from('tickets')
+  const { data: ticket } = await supabase.from('tickets')
     .select()
+    .eq('id', params.id)
+    .single()
 
-  if (error) {
-    console.log(error.message)
+  return {
+    title: `Dojo Helpdesk | ${ticket?.title || 'Ticket not Found'}`
+  }
+}
+
+async function getTicket(id) {
+  const supabase = createServerComponentClient({ cookies })
+
+  const { data } = await supabase.from('tickets')
+    .select()
+    .eq('id', id)
+    .single()
+
+  if (!data) {
+    notFound()
   }
 
   return data
 }
 
+export default async function TicketDetails({ params }) {
+  const ticket = await getTicket(params.id)
 
-export default async function ticketList() {
-  //fetch data
-  const tickets =await getTickets();
-    return (
-    <>
-      {tickets.map((ticket)=>(
-        <div key={ticket.id} className="card my-5">
-           <Link href={`/tickets/${ticket.id}`}>
-            <h3>{ticket.title}</h3>
-            <p>{ticket.body.slice(0,200)}...</p>
-            <div className={`pill ${ticket.priority}`}>
-                {ticket.priority} priority
-            </div>
-            </Link>
+  return (
+    <main>
+      <nav>
+        <h2>Ticket Details</h2>
+      </nav>
+      <div className="card">
+        <h3>{ticket.title}</h3>
+        <small>Created by {ticket.user_email}</small>
+        <p>{ticket.body}</p>
+        <div className={`pill ${ticket.priority}`}>
+          {ticket.priority} priority
         </div>
-      ))
-      }
-      {tickets.length===0 &&( 
-        <p className="text-center">There are no open tickets ,yay!!</p>
-      )}
-    </>
+      </div>
+    </main>
   )
 }
-
-
-///tickets/id
